ecefcd1c5a92ecd7f8babdf8e4696baebaa8a112
DinushiJayasekara <jkdrj02@gmail.com>
2020-11-10 22:04:59 +0530

    start-layouts

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,101 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:skype_clone/utils/universal_variables.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      backgroundColor: UniversalVariables.blackColor,
+      body: PageView(
+        children: <Widget>[
+          Center(
+              child: Text(
+            'Chat List Screen',
+            style: TextStyle(color: Colors.white),
+          )),
+          Center(
+              child: Text(
+            'Call Logs',
+            style: TextStyle(color: Colors.white),
+          )),
+          Center(
+              child: Text(
+            'Contact Screen',
+            style: TextStyle(color: Colors.white),
+          )),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: CupertinoTabBar(
+            backgroundColor: UniversalVariables.blackColor,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.chat,
+                    color: (_page == 0)
+                        ? UniversalVariables.lightBlueColor
+                        : UniversalVariables.greyColor,
+                  ),
+                  label: 'Chats',
+                  backgroundColor: (_page == 0)
+                      ? UniversalVariables.lightBlueColor
+                      : Colors.grey),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.call,
+                    color: (_page == 1)
+                        ? UniversalVariables.lightBlueColor
+                        : UniversalVariables.greyColor,
+                  ),
+                  label: 'Call',
+                  backgroundColor: (_page == 1)
+                      ? UniversalVariables.lightBlueColor
+                      : Colors.grey),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.contact_page,
+                    color: (_page == 2)
+                        ? UniversalVariables.lightBlueColor
+                        : UniversalVariables.greyColor,
+                  ),
+                  label: 'Contacts',
+                  backgroundColor: (_page == 2)
+                      ? UniversalVariables.lightBlueColor
+                      : Colors.grey),
+            ],
+            onTap: navigationTapped,
+            currentIndex: _page,
+          ),
+        ),
       ),
     );
   }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:skype_clone/resources/firebase_repository.dart';
 import 'package:skype_clone/screens/home_screen.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:skype_clone/utils/universal_variables.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -11,29 +13,52 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   FirebaseRepository _repository = FirebaseRepository();
 
+  bool isLoginPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: loginButton(),
+      backgroundColor: UniversalVariables.blackColor,
+      body: Stack(
+        children: [
+          Center(
+            child: loginButton(),
+          ),
+          isLoginPressed
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Container()
+        ],
+      ),
     );
   }
 
   Widget loginButton() {
-    return RaisedButton(
-      padding: EdgeInsets.all(35.0),
-      child: Text(
-        'Login',
-        style: TextStyle(
-          fontSize: 35,
-          fontWeight: FontWeight.w900,
-          letterSpacing: 1.2,
+    return Shimmer.fromColors(
+      baseColor: Colors.white,
+      highlightColor: UniversalVariables.senderColor,
+      child: FlatButton(
+        padding: EdgeInsets.all(35.0),
+        child: Text(
+          'Login',
+          style: TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.w900,
+            letterSpacing: 1.2,
+          ),
         ),
+        onPressed: () => performLogin(),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       ),
-      onPressed: () => performLogin(),
     );
   }
 
   void performLogin() {
+    setState(() {
+      isLoginPressed = true;
+    });
+
     _repository.signIn().then((User user) {
       if (user != null) {
         authenticateUser(user);
@@ -45,6 +70,9 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void authenticateUser(user) {
     _repository.authenticateUser(user).then((isNewUser) => {
+          setState(() {
+            isLoginPressed = false;
+          }),
           if (isNewUser)
             {
               _repository.addDataToDb(user).then((value) => {
